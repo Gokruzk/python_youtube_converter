a0e0edfa73a279d80a44b1bd0e816bc951d2bfd7
Gokruzk <nigell@gmail.com>
2024-04-07 20:57:13 -0500

    popups module

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from tkinter import *
-from tkinter import messagebox as Messagebox
 from download_video import action
+from popup import popups
 
 
 def main():
-    def popup():
-        Messagebox.showinfo('About me', 'Nigell Marcel Jama oyarvide\n2024')
 
     root = Tk()
     root.config(bd=15)
@@ -15,16 +13,17 @@ def main():
     root.config(menu=menubar)
     helpmenu = Menu(menubar, tearoff=0)
     menubar.add_cascade(label='About', menu=helpmenu)
-    helpmenu.add_command(label='About me', command=popup)
+    helpmenu.add_command(label='About me', command=lambda: popups('About me', 'Nigell Marcel Jama Oyarvide'))
     menubar.add_command(label='Exit', command=root.destroy)
 
-    instrucciones = Label(root, text='Convert videos from YouTube to mp3\n\nInsert url here')
+    instrucciones = Label(
+        root, text='Convert videos from YouTube to mp3\n\nInsert url here')
     instrucciones.grid(row=0, column=1)
 
-    videos = Entry(root)
-    videos.grid(row=1, column=1)
+    url = Entry(root)
+    url.grid(row=1, column=1)
 
-    button = Button(root, text='Download', command=lambda: action(videos))
+    button = Button(root, text='Download', command=lambda: action(url))
     button.grid(row=2, column=2)
 
     root.mainloop()
---
+++ popup.py
@@ -0,0 +1,5 @@
+from tkinter import messagebox as Messagebox
+
+
+def popups(title, text):
+    Messagebox.showinfo(title, text)
