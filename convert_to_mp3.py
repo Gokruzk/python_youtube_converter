6a3802b5794fc283ac6e0ce91692d81e5a67a769
Gokruzk <nigell@gmail.com>
2024-05-18 23:39:51 -0500

    fix: :adhesive_bandage: mp3 to mp4 conversion

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -1,16 +1,29 @@
+import os
 import subprocess
 from popup import popups
+from pathlib import Path
+
+home = Path.home()
+download_folder = Path(home, "Music Downloaded")
+os.chdir(download_folder)
 
 
 def convert(input_file, output_file):
     # ffmpeg command to convert .mp4 to .mp3
     popups('Conversion', 'Converting to .mp3')
-    ffmpeg_cmd = f'''ffmpeg -i {
-        input_file} -vn -acodec libmp3lame -ab 192000 -ar 44100 -y {output_file}'''
+    ffmpeg_cmd = [
+        'ffmpeg',
+        '-i', str(input_file),
+        '-vn',
+        '-acodec', 'libmp3lame',
+        '-ab', '192000',
+        '-ar', '44100',
+        '-y', str(output_file)
+    ]
     try:
-        #execute command
-        subprocess.run(f'{ffmpeg_cmd}', shell=False)
-        #show message
-        popups('Sucess', 'Successfully converted')
+        # execute command
+        result = subprocess.run(ffmpeg_cmd, check=True)
+        # show message
+        popups('Success', 'Successfully converted')
     except subprocess.CalledProcessError as e:
-        print(e)
+        print(f'Error during conversion: {e}')
