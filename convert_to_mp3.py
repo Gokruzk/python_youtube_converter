bff06d0e4c98e696f9baddc62021d99298273152
Gokruzk <nigell@gmail.com>
2024-05-18 22:41:52 -0500

    fix: :adhesive_bandage: command string

--- convert_to_mp3.py
+++ convert_to_mp3.py
@@ -5,8 +5,8 @@ from popup import popups
 def convert(input_file, output_file):
     # ffmpeg command to convert .mp4 to .mp3
     popups('Conversion', 'Converting to .mp3')
-    ffmpeg_cmd = f'ffmpeg -i {
-        input_file} -vn -acodec libmp3lame -ab 192000 -ar 44100 -y {output_file}'
+    ffmpeg_cmd = f'''ffmpeg -i {
+        input_file} -vn -acodec libmp3lame -ab 192000 -ar 44100 -y {output_file}'''
     try:
         #execute command
         subprocess.run(f'{ffmpeg_cmd}', shell=False)
